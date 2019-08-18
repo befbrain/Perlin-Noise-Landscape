f40427bf509413cbd5adafa44644716c84284267
Brandon Feder <befbrain@gmail.com>
2019-08-18 15:43:19 -0400

    You can now move around and see how the landscape changes

--- src/Landscape.java
+++ src/Landscape.java
@@ -6,7 +6,11 @@ import java.util.Arrays;
 
 public class Landscape extends PApplet{
 
-    public float[][] grid = new float[200][200];
+    public float[][] grid = new float[1000][1000];
+    public int[] translateAmount = {0, 0};
+    public int vert = 0;
+    public int horz = 0;
+
 
     public static void main(String[] args) {
 
@@ -18,63 +22,85 @@ public class Landscape extends PApplet{
     }
 
     public void setup() {
-        frameRate(1.0f);
-        float noiseFactor = 0.05f;
+        frameRate(60f);
 
-        for(int x = 0; x < 200; x++)
-            for(int y = 0; y < 199; y++)
-                grid[x][y] = noise(x*noiseFactor, y*noiseFactor);
     }
 
     public void draw() {
+
+        translateAmount[0] -= vert;
+        translateAmount[1] -= horz;
         noStroke();
+        clear();
 
         rotateX(PI / 4);
-        scale(0.4f);
+        scale(0.5f);
         translate(600, 1000);
 
-        for(int x = 0; x < 79; x++) {
-            beginShape(TRIANGLE_STRIP);
-            for(int y = 1; y < 80; y++) {
-                fill( grid[x][y] * 300);
+        float noiseFactor = 0.05f;
 
-                if(190 < grid[x][y] * 300) {
+        for(int x = 0; x < 1000; x++)
+            for(int y = 0; y < 1000; y++)
+                grid[x][y] = noise(x*noiseFactor + translateAmount[0]*noiseFactor, y*noiseFactor + translateAmount[1]*noiseFactor);
+
+        for (int x = 0; x < 100; x++) {
+            beginShape(TRIANGLE_STRIP);
+            for (int y = 1; y < 100; y++) {
+                fill(grid[x][y] * 300);
+
+                if (190 < grid[x][y] * 300) {
                     fill(235, 235, 235);
                     stroke(255, 255, 255);
                 } else if (170 < grid[x][y] * 300) {
                     fill(100, 100, 100);
                     stroke(80, 80, 80);
                 } else if (120 < grid[x][y] * 300) {
-                    fill(34,139,34);
+                    fill(34, 139, 34);
                     stroke(14, 119, 14);
                 } else if (100 < grid[x][y] * 300) {
-                    fill(240,230,140);
+                    fill(240, 230, 140);
                     stroke(220, 210, 120);
                 } else {
                     fill(0, 100, 255);
                     stroke(0, 80, 235);
                 }
 
-                vertex(4 + 10*(x+0), 4 + 10*(y+0), grid[x][y] * 310);
-                vertex(4 + 10*(x+1), 4 + 10*(y), grid[x+1][y] * 310);
+                vertex(4 + 10 * (x + 0) - 300, 4 + 10 * (y + 0) - 500, grid[x][y] * 310);
+                vertex(4 + 10 * (x + 1) - 300, 4 + 10 * (y) - 500, grid[x + 1][y] * 310);
             }
             endShape(CLOSE);
         }
+    }
 
-//        beginShape(TRIANGLE_STRIP);
-//
-//        //  x: 0    y: 0
-//        vertex(10 + 0, 10 + 0);
-//        vertex(10 + 0, 10 + 8);
-//
-//        //  x: 0    y: 0
-//        vertex(10 + 8, 10 + 0);
-//        vertex(10 + 8, 10 + 8);
-//
-//        //  x: 0    y: 0
-//        vertex(10 + 16, 10 + 0);
-//        vertex(10 + 16, 10 + 8);
-//        endShape();
+    public void keyPressed() {
+        if(keyCode == LEFT) {
+            vert = 1;
+            horz = 0;
+        } else if(keyCode == RIGHT) {
+            vert = -1;
+            horz = 0;
+        } else if(keyCode == UP) {
+            horz = 1;
+            vert = 0;
+        } else if(keyCode == DOWN) {
+            horz = -1;
+            vert = 0;
+        }
+    }
 
+    public void keyReleased() {
+        if(keyCode == LEFT) {
+            vert = 0;
+            horz = 0;
+        } else if(keyCode == RIGHT) {
+            vert = 0;
+            horz = 0;
+        } else if(keyCode == UP) {
+            vert = 0;
+            horz = 0;
+        } else if(keyCode == DOWN) {
+            vert = 0;
+            horz = 0;
+        }
     }
 }
